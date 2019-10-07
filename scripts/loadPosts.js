312abb7b3fb417072a5a05793a31686b9cc1ec2a
ctuimolo <tuimoloau@gmail.com>
2019-10-06 18:12:42 -0700

    Started buttons and ajax handling loading multiple posts

--- scripts/loadPosts.js
+++ scripts/loadPosts.js
@@ -1,10 +1,18 @@
-import {URL} from "./metadata.js";
-
 // this is the current post count from Post Index
+
+var params = $("script[src*='./scripts/loadPosts.js']");
+
 var postCount = 0;
 var posts = [];
-var postListSize = 6;
 var postListPromises = [];
+var defaultPostListSize = 6;
+//var startIndex = parseInt(params.attr('startIndex'));
+//var startIndex = 0;
+
+function getPostID(post)
+{
+    return post["ID"];
+}
 
 function formatPostTitle(post)
 {
@@ -39,51 +47,64 @@ function formatPostBody(post)
     );
 }
 
+
+
 function getPost(postIndex, postID)
 {
-    return $.getJSON(URL+"/data/posts/"+postIndex["posts"][postID]+".json", function(post) 
+    var request = $.getJSON("../../data/posts/"+postIndex["posts"][postID]+".json", function(post) 
     {
         posts[postIndex["postCount"]-postID] = post;
-    });
+    })
+
+    return request;
 }
 
-$(document).ready(function() 
+function writePosts(startIndex, postListSize = defaultPostListSize)
 {
-    $.getJSON(URL+"/data/postIndex.json", function(postIndex) 
+    $.getJSON("../../data/postIndex.json", function(postIndexRaw) 
     {
-        postCount = postIndex["postCount"];
+        postCount = postIndexRaw["postCount"];
     })
-    
+
     .then(function (postIndex) 
     {
         for (var i = 0; i < postListSize; i++)
         {
-            var request = getPost(postIndex, postCount - i);
-            postListPromises.push(request);
+            if(startIndex + i < postCount)
+            {
+                var request = getPost(postIndex, postCount - i - startIndex);
+                postListPromises.push(request);
+            }
         };
 
-        $.when.apply(null, postListPromises).done(function()
-        {  
+        $.when.apply(null, postListPromises).done(function() 
+        {
+            $("#postlist").empty();
             for(var i = 0; i < postListSize; i++)
             {
-                if(posts[i] != null) 
+                if(posts[i+startIndex] != null) 
                 {
                     $("#postList").append(
                         "<div class='post'>" +
-                            "<text>===== POST #"+i+"</text>"+
-                            formatPostTitle(posts[i])+
-                            formatPostDate(posts[i])+
-                            formatPostBody(posts[i])+
+                            "<text>===== POST #"+(i+startIndex)+" === ID: "+getPostID(posts[i+startIndex])+" ====="+"</text>"+
+                            formatPostTitle(posts[i+startIndex])+
+                            formatPostDate(posts[i+startIndex])+
+                            formatPostBody(posts[i+startIndex])+
                         "</div>"
                     );
                 } else {
                     $("#postList").append(
                         "<div class='post'>" +
-                            "<text> === ERROR POSTLIST INDEX: " + i + " === </text>" +
+                            "<text> === ERROR POSTLIST INDEX: " + (i+startIndex) + " === </text>" +
                         "</div>"
                     );
                 };
-            };
-        });
+            }
+        })
     });
+}
+ 
+$(document).ready(function() 
+{
+    writePosts(0);
 });
