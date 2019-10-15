0c3c07bb315fb4288bcf7e516f8ced74be8e0355
ctuimolo <tuimoloau@gmail.com>
2019-10-15 01:57:47 -0700

    cleaned pages and added header page JS

---
+++ scripts/loadPageHeader.js
@@ -0,0 +1,22 @@
+ $('.pageHeader').append(
+    "<a href='/'>"+
+        "<img class='banner' src='./images/banner.png'></img>"+
+    "</a>"+
+    "<ul class='headerNavBar'>"+
+        "<li class='headerNavBarButton'>"+
+            "<text>[&nbsp</text>"+
+                "<a class='headerLink' href='/'>ホーム:home</a>"+
+            "<text>&nbsp]</text>"+
+        "</li>"+
+        "<li class='headerNavBarButton'>"+
+            "<text>[&nbsp</text>"+
+                "<a class='headerLink' href='/'>ギャラリー:gallery</a>"+
+            "<text>&nbsp]</text>"+
+        "</li>"+
+        "<li class='headerNavBarButton'>"+
+            "<text>[&nbsp</text>"+
+                "<a class='headerLink' href='/archive'>アーカイブ:archive</a>"+
+            "<text>&nbsp]</text>"+
+        "</li>"+
+    "</ul>"
+);
